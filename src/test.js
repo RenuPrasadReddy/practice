590ca5b8f639aef6ea9b05b11fae0a771b8f6c6a
renu <shubham.srivastava@insured.mine.com>
2020-07-05 17:36:14 +0530

    d2 changes

--- src/test.js
+++ src/test.js
@@ -7,7 +7,7 @@ console.log("feature-a... developer 1");
 console.log("feature-a... developer 2");
 console.log("feature-a in master... developer 2");
 
-console.log("feature-a in fa... developer 2");
+console.log("feature-a in fa222... developer 2");
 
 
 
