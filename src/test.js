26def414c9d0244b9c31270cb908c9fb374187f1
renu <shubham.srivastava@insured.mine.com>
2020-07-12 23:22:37 +0530

    added line 8

--- src/test.js
+++ src/test.js
@@ -5,5 +5,6 @@ console.log("feature-a...");
 console.log("feature-a...");
 console.log("feature-a... developer 1");
 console.log("feature-a... developer 2");
+console.log("test maegre 2... developer 2");
 
 
